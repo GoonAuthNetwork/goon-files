cd8507abf10148efa2db9f57ce4285b002adcd5b
Ian P <ian@aotc.us>
2021-03-29 22:10:06 -0400

    changed the default logger for fastapi to loguru

--- app/config.py
+++ app/config.py
@@ -10,19 +10,36 @@ if DEVELOPMENT:
     load_dotenv()
 
 # Database settings
-DB_MIN_CONNECTIONS_COUNT = int(os.environ.get("DB_MIN_CONNECTIONS_COUNT", "10"))
-DB_MAX_CONNECTIONS_COUNT = int(os.environ.get("DB_MAX_CONNECTIONS_COUNT", "10"))
+DB_MIN_CONNECTIONS_COUNT = int(os.getenv("DB_MIN_CONNECTIONS_COUNT", 10))
+DB_MAX_CONNECTIONS_COUNT = int(os.getenv("DB_MAX_CONNECTIONS_COUNT", 10))
 
-DB_CONNECTION_URL = os.environ.get("DB_CONNECTION_URL")
+DB_CONNECTION_URL = os.getenv("DB_CONNECTION_URL")
 
 if DB_CONNECTION_URL is None:
-    DB_MONGO_HOST = os.environ.get("DB_MONGO_HOST", "127.0.0.1")
-    DB_MONGO_PORT = int(os.environ.get("DB_MONGO_PORT", "27017"))
-    DB_MONGO_USER = os.environ.get("DB_MONGO_USER", "root")
-    DB_MONGO_PASS = os.environ.get("DB_MONGO_PASS", "password")
-    DB_MONGO_DB_NAME = os.environ.get("DB_MONGO_DB_NAME", "goon-files")
+    DB_MONGO_HOST = os.getenv("DB_MONGO_HOST", "127.0.0.1")
+    DB_MONGO_PORT = int(os.getenv("DB_MONGO_PORT", 27017))
+    DB_MONGO_USER = os.getenv("DB_MONGO_USER", "root")
+    DB_MONGO_PASS = os.getenv("DB_MONGO_PASS", "password")
+    DB_MONGO_DB_NAME = os.getenv("DB_MONGO_DB_NAME", "goon-files")
 
     DB_CONNECTION_URL = (
         f"mongodb://{DB_MONGO_USER}:{DB_MONGO_PASS}@"
         + f"{DB_MONGO_HOST}:{DB_MONGO_PORT}/{DB_MONGO_DB_NAME}"
     )
+
+# Logger settings
+LOGGING_CONFIG = {
+    "file": {
+        "path": os.getenv("LOGGING_FILE_PATH", "/var/logs"),
+        "name": os.getenv("LOGGING_FILE_NAME", "/access.log"),
+        "rotation": os.getenv("LOGGING_FILE_ROTATION", "20 days"),
+        "retention": os.getenv("LOGGING_FILE_PETENTION", "1 months"),
+    },
+    "level": os.getenv("LOGGING_LEVEL", "info"),
+    "format": os.getenv(
+        "LOGGING_FORMAT",
+        "<level>{level: <8}</level> <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>"
+        + " request id: {extra[request_id]} - <cyan>{name}</cyan>:"
+        + "<cyan>{function}</cyan> - <level>{message}</level>",
+    ),
+}
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from .mongodb import connect_to_mongo, close_mongo_connection
+from .fastapi_logging import CustomizeLogger
 
 app = FastAPI(title="Goon Files")
+app.logger = CustomizeLogger.make_logger()
 
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)

09a386da6755babb2bcacdc795120c2f692441a3
Ian P <ian@aotc.us>
2021-03-30 11:26:24 -0400

    cleaned up logging

---
+++ app/logging_hook.py
@@ -0,0 +1,75 @@
+import logging
+import sys
+from pathlib import Path
+from loguru import logger
+from .config import LOGGING_CONFIG
+
+"""
+This file essentually taken from:
+https://medium.com/1mgofficial/how-to-override-uvicorn-logger-in-fastapi-using-loguru-124133cdcd4e
+"""
+
+
+class InterceptHandler(logging.Handler):
+    loglevel_mapping = {
+        50: "CRITICAL",
+        40: "ERROR",
+        30: "WARNING",
+        20: "INFO",
+        10: "DEBUG",
+        0: "NOTSET",
+    }
+
+    def emit(self, record):
+        try:
+            level = logger.level(record.levelname).name
+        except AttributeError:
+            level = self.loglevel_mapping[record.levelno]
+
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+def customize_logging():
+
+    """
+    filepath: Path = Path.joinpath(
+        LOGGING_CONFIG["file"]["path"], LOGGING_CONFIG["file"]["name"]
+    )
+    """
+
+    filepath: Path = None
+    level: str = LOGGING_CONFIG["level"]
+    rotation: str = LOGGING_CONFIG["file"]["rotation"]
+    retention: str = LOGGING_CONFIG["file"]["retention"]
+    format: str = LOGGING_CONFIG["format"]
+
+    logger.remove()
+    logger.add(
+        sys.stdout, enqueue=True, backtrace=True, level=level.upper(), format=format
+    )
+    if filepath is not None:
+        logger.add(
+            str(filepath),
+            rotation=rotation,
+            retention=retention,
+            enqueue=True,
+            backtrace=True,
+            level=level.upper(),
+            format=format,
+        )
+
+    logging.basicConfig(handlers=[InterceptHandler()], level=0)
+
+    for _log in ["uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"]:
+        _logger = logging.getLogger(_log)
+        _logger.propagate = False
+        _logger.handlers = [InterceptHandler()]
+
+    return logger.bind(request_id=None, method=None)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from .mongodb import connect_to_mongo, close_mongo_connection
-from .fastapi_logging import CustomizeLogger
+from .logging_hook import customize_logging
 
 app = FastAPI(title="Goon Files")
-app.logger = CustomizeLogger.make_logger()
+app.logger = customize_logging()
 
 app.add_event_handler("startup", connect_to_mongo)
 app.add_event_handler("shutdown", close_mongo_connection)
